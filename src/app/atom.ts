1b6653634520b589e325b016ae650adc48f0bb1e
AyushScripts <ayushtheking117@gmail.com>
2024-02-21 21:12:35 +0530

    ClimaCast: A weather forecasting app made with ReactJs, NextJs, TypeScript & TailwindCSS.

---
+++ src/app/atom.ts
@@ -0,0 +1,7 @@
+// for implement all global states, atom.ts file is used
+
+
+import { atom } from "jotai";
+
+export const placeAtom = atom("Republic of India");
+export const loadingCityAtom = atom(false);
